0deffcede4778062292dc14292f82256d4dfdefa
ok.abbasova@yandex.ru <kotlova@dot-dot.ru>
2023-06-29 22:39:54 +0300

    final task_ura_ya_molodec

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,21 +33,27 @@
 //    }
 // }
 
+
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    let data = {
+    client: name + ' ' + phone,
+    order: {
+    address: 'ул. ' + address.street + ', дом ' + address.house + ', ' + address.entrance
+    + ' подъезд, ' + address.floor + ' этаж, кв. ' + address.flat,
+    sum: sum
+    },
+    goods: []
+    };
+   
+    for (let i = 0; i < goods.length; i++) {
+        let item = {
+            title: goods[i].title,
+            count: goods[i].count
+        };
+        data.goods.push(item);
     }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
+    
+    let jsonData = JSON.stringify({ data: data });
+    
     return jsonData;
-}
+    }
